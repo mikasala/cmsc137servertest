d080e3e69dde27854bb978e37316fd429ea6059f
mikasala <migkasala@gmail.com>
2013-10-01 10:52:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,22 +5,26 @@
 		if($_POST["user"]!=NULL && $_POST["password"]!=NULL){
 			$pw = $_POST["password"];
 			$user = $_POST["user"];
-			if(checkUser($user,$pw)==1){
+			if(checkUser($user)==1){ // if username exists inthe database
 				$password = getPassword($user);
-				if($pw==$password){
-					$_SESSION["user"] = $user;
+				if($pw==$password){ // if the password input match the password in the database
+					$_SESSION["user"] = $user; // sets the SESSION user
 					header('Location: profile.php');
 				}else echo "Invalid Username or Password.".'<br/><br/>';
 			}else echo "Invalid Username or Password.".'<br/><br/>';
 		}
 	}
+	
+	$con=connect();
+	echo ($con)?"YEAH!":"HELL!";
+	echo '<br/><br/>';
 ?>
 <html>
 	<head>
 		<title>Server Test</title>
 	</head>
 	<body>
-		<fieldset>
+		<fieldset style="width:40%;">
 			<legend>Log In</legend>
 			<form method="post" action="">
 				Userame: <input type="text" name="user" id="user"/><br/>
